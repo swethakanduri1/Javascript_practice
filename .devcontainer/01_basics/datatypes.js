4acd38cb43d7fb2340c55ffe373c375a98bcfc0a
swethakanduri1 <153656203+swethakanduri1@users.noreply.github.com>
2024-11-05 18:10:59 -0500

    Create datatypes.js

---
+++ .devcontainer/01_basics/datatypes.js
@@ -0,0 +1,9 @@
+console.log("hi")
+let s1=5
+let s2=6
+let s3=7
+console.table([s1,s2,s3])
+let s4="sghag"
+let s5=null
+console.log(typeof s5);
+// type value of null data types in object.Rest of all data types have thier own name displayed
