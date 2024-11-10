b070190e78a7d979687ef0a797de4380ba43215e
swethakanduri1 <153656203+swethakanduri1@users.noreply.github.com>
2024-11-10 15:27:11 -0500

    Update datatypes.js

--- .devcontainer/01_basics/datatypes.js
+++ .devcontainer/01_basics/datatypes.js
@@ -7,3 +7,8 @@ let s4="sghag"
 let s5=null
 console.log(typeof s5);
 // type value of null data types in object.Rest of all data types have thier own name displayed
+let cd=null
+let gh=undefined or let gh
+let gh=0;
+//there three are different.0 meanrs value is 0,null means value is empty and undefined means valus is not yet defined.
+  
