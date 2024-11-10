8b3d37b62891796efa1b1706b1c7847eff1afef2
swethakanduri1 <153656203+swethakanduri1@users.noreply.github.com>
2024-11-10 17:48:22 -0500

    Update datatypes.js

--- .devcontainer/01_basics/datatypes.js
+++ .devcontainer/01_basics/datatypes.js
@@ -11,4 +11,7 @@ let cd=null
 let gh=undefined or let gh
 let gh=0;
 //there three are different.0 meanrs value is 0,null means value is empty and undefined means valus is not yet defined.
-  
+ // bit int is used if number is too long and has a n at the end.The default datatype for numbers is doubleint.
+let gyu=88379720898239238293782467365874370n
+//return type of all non primitive datatypes like arrays,objects, is obj
+
