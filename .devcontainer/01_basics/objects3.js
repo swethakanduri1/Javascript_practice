2b37f42303a02ca074e104732a45f813ceafed36
swethakanduri1 <153656203+swethakanduri1@users.noreply.github.com>
2024-11-17 12:55:32 -0500

    Create objects3.js

---
+++ .devcontainer/01_basics/objects3.js
@@ -0,0 +1,13 @@
+//object destructuring means instead of using objname.property every time we use aloas namr like instructor
+const course = {
+    coursename: "js in hindi",
+    price: "999",
+    courseInstructor: "hitesh"
+}
+
+// course.courseInstructor
+
+const {courseInstructor: instructor} = course
+
+// console.log(courseInstructor);
+console.log(instructor);
