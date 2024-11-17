d44844d147fe13c7dd219d19921322eea7f99248
swethakanduri1 <153656203+swethakanduri1@users.noreply.github.com>
2024-11-17 12:31:41 -0500

    Create objects1.js

---
+++ .devcontainer/01_basics/objects1.js
@@ -0,0 +1,77 @@
+// const tinderUser = new Object()// gives {}
+const tinderUser = {}=gives {}
+
+tinderUser.id = "123abc"
+tinderUser.name = "Sammy"
+tinderUser.isLoggedIn = false
+
+// console.log(tinderUser);
+
+const regularUser = {
+    email: "some@gmail.com",
+    fullname: {
+        userfullname: {
+            firstname: "hitesh",
+            lastname: "choudhary"
+        }
+    }
+}
+
+// console.log(regularUser.fullname.userfullname.firstname);
+
+const obj1 = {1: "a", 2: "b"}
+const obj2 = {3: "a", 4: "b"}
+const obj4 = {5: "a", 6: "b"}
+// for combining objects it is best to use spread operator like in arrays.we can also use assign like concate operator
+// const obj3 = { obj1, obj2 }
+// const obj3 = Object.assign({}, obj1, obj2, obj4)
+
+const obj3 = {...obj1, ...obj2}
+// console.log(obj3);
+
+// we get like this frm  databases
+const users = [
+    {
+        id: 1,
+        email: "h@gmail.com"
+    },
+    {
+        id: 1,
+        email: "h@gmail.com"
+    },
+    {
+        id: 1,
+        email: "h@gmail.com"
+    },
+]
+
+users[1].email
+// console.log(tinderUser);
+
+// console.log(Object.keys(tinderUser)); returns array of keys and can be looped
+// console.log(Object.values(tinderUser));
+// console.log(Object.entries(tinderUser));returns indivual array of hey value pairs within a array
+
+// console.log(tinderUser.hasOwnProperty('isLoggedIn'));=false
+
+
+const course = {
+    coursename: "js in hindi",
+    price: "999",
+    courseInstructor: "hitesh"
+}
+
+// course.courseInstructor
+
+const {courseInstructor: instructor} = course
+
+// console.log(courseInstructor);
+console.log(instructor);
+
+
+
+[
+    {},
+    {},
+    {}
+]
